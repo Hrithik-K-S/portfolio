bab1849f659b767a19dcd1399c786324eb142d01
Hrithik-K-S <97302423+Hrithik-K-S@users.noreply.github.com>
2023-03-13 00:32:32 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,11 +2,29 @@ import streamlit as st
 import linear
 import logistic
 import powerbi
+import requests
+from PIL import Image
+from io import BytesIO
+import toml
+#https://hrithik-k-s-portfolio-home-34095i.streamlit.app/
+# Define repository and file paths
+
+#     #https://github.com/Hrithik-K-S/portfolio/blob/main/slide1.png
+# response = requests.get(url)
+
+# # Get contents of file
+
+# # Decode contents from base64 and load as TOML
+# config = toml.loads(response.text)
+
+# # Access config values
+# my_value = config["section"]["my_key"]
+
 
 st.set_page_config(
     page_title="My Resume",
     page_icon=":memo:",
-    layout="wide",
+    layout="wide"
     #initial_sidebar_state="collapsed",
 )
 
@@ -19,6 +37,17 @@ def show_main_page():
     st.markdown(
         """
         <style>
+            .stSideBar {
+                    position: relative !important;
+            }
+            .stApp > main {
+                    position: relative;
+            }
+            .text {
+                    position: absolute;
+                    top: 50px;
+                    left: 50px;
+            }
             .header {
                 font-size: 2.5rem;
                 font-weight: bold;
@@ -59,51 +88,139 @@ def show_main_page():
 
     with st.container():
        # st.markdown('# Personal Information #')
-        st.write("<div style='display: flex; justify-content: center; align-items: center; flex-direction: column; text-align: center; color: #ffffff; font-size:45px; background-color: #EBECF0; font-family: Franklin Gothic Medium (Headings); height: 50vh;'>\
-                    <div>HRITHIK K S</div>\
-                    <div style='font-size: 18px;'>hrithik.ks@outlook.com</div>\
-                    <div style='font-size: 18px;'>K R Puram, Bangalore 560036</div>\
-                    </div>", unsafe_allow_html=True)
+       
+        background_image_path = r"C:\Users\hrith\Desktop\linear_regression\cover-image(1).png"
+
+        st.write(
+            f"""
+            <div style='display: flex; justify-content: space-between; align-items: center; flex-direction: row; text-align: center; 
+                        color: #ffb300; font-size: 45px; background-image: url("{background_image_path}"); 
+                        background-repeat: no-repeat; background-size: cover; font-family: Franklin Gothic Medium (Headings); 
+                        height: 50vh;'>
+                <div>
+                    <strong>HRITHIK K S</strong><br>
+                    <div style='font-size: 18px;'>hrithik.ks@outlook.com</div>
+                    <div style='font-size: 18px;'>+91 8546926069</div>
+                </div>
+                <div style='display: flex; justify-content: flex-end;'>
+                    <img src='https://raw.githubusercontent.com/Hrithik-K-S/Portfolio/main/profile-pic(4).png' width=230>
+                </div>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+
+
         # st.write('Email: hrithik.ks@outlook.com')
         # st.write('Phone: 8546926069')
+    table_code = """
+        <table style='border-collapse: collapse;'>
+            <thread>
+                <tr>
+                    <th><div style="text-align:right"><h2>Skills & Abilities</h2></div></th>
+                    <th><div style="text-align:left"><h2>Objective</h2></div</th>
+                </tr>
+            </thread>
+            <thread>
+                <tr>
+                    <td>Programming: Python,Libraries: Pandas, NumPy, Matplotlib, Scikit-learn,Databases: MySQL, MS SQL,Visualization: Excel, PowerPoint, Power BI,Operating Systems: Windows</td>
+                    <td> A highly motivated and analytical data science professional seeking to leverage my strong technical skills, and passion for problem-solving to contribute to the success of an organization in the field of data science.\
+        I aim to generate valuable insights that drive decision-making and support business growth.</td>
+                </tr>
+            </thread>
+            <thread>
+                <tr>
+                    <th><div style="text-align:right"><h2>Education</h2></div></th>
+                    <th><div style="text-align:left"><h2>Projects</h2></div</th>
+                </tr>
+            </thread>
+            <thread>
+                <tr>
+                    <td>VEMANA INSTITUTE OF TECHNOLOGY\
+                        BACHELOR’S IN MECHANICAL ENGINEERING\
+                        Scored CGPA of 7.</td>
+                    <td>Car price prediction using Linear regression.\
+                        Heat attack prediction using logistic regression.\
+                        Exploratory data analysis on Games sales with charts using POWERBI.\
+                        Engineering Project - I designed a cassette ceiling air conditioning system that includes integrated lighting, speakers, and a projector, with voice-activated features.</td>
+                </tr>        
+            </thread>
+            <thread>
+                <tr>
+                    <th><div style="text-align:right"><h2>Courses</h2></div></th>
+                    <th><div style="text-align:left"><h2>Experience</h2></div</th>
+                </tr>
+            </thread>
+            <thread>
+                <tr>
+                    <td>
+                    Google Data Analytics Professional Certificate, Coursera\
+                    Python 101 for Data Science, Cognitiveclass.ai, Powered by IBM\
+                    Vehicle Dynamics - ETG </td>
 
-    with st.container():
-        st.markdown('## Objective')
-        st.write('A highly motivated and analytical data science professional seeking to leverage my strong technical skills, and passion for problem-solving to contribute to the success of an organization in the field of data science. I aim to generate valuable insights that drive decision-making and support business growth.')
+                    <td>
+                    ok </td>
+                </tr>
+            </thread>
 
-    with st.container():
-        st.markdown('## Skills & Abilities')
-        st.write('Programming: Python')
-        st.write('Libraries: Pandas, NumPy, Matplotlib, Scikit-learn.')
-        st.write('Databases: MySQL, MS SQL')
-        st.write('Visualization: Excel, PowerPoint, Power BI')
-        st.write('Operating Systems: Windows')
+        </table>
+    """
+    st.write(table_code, unsafe_allow_html=True)
+    #streamlit run import_streamlit_as_st.py
+    left,right = st.columns(2, gap='small') 
 
-    with st.container():
-        st.markdown('## Projects')
-        st.write('### Car price prediction using Linear regression.')
-        st.write('### Heat attack prediction using logistic regression.')
-        st.write('### Exploratory data analysis on Games sales with charts using POWERBI.')
-        st.write('### Engineering Project - I designed a cassette ceiling air conditioning system that includes integrated lighting, speakers, and a projector, with voice-activated features.')
+    with right:
 
-    with st.container():
-        st.markdown('## Education')
-        st.write('VEMANA INSTITUTE OF TECHNOLOGY')
-        st.write('BACHELOR’S IN MECHANICAL ENGINEERING')
-        st.write('Scored CGPA of 7.')
+        st.markdown('<div style="text-align:left"><h2>Objective</h2></div>', unsafe_allow_html=True)
+        st.write('<div style="display: flex; justify-content: left; align-items: left; text-align: left; text-wrap: avoid;">A highly motivated and analytical data science professional seeking to leverage my strong technical skills, and passion for problem-solving to contribute to the success of an organization in the field of data science.\
+        I aim to generate valuable insights that drive decision-making and support business growth.</div>', unsafe_allow_html=True)
+    
+    with left:
 
-    with st.container():
-        st.markdown('## Experience')
-        st.write('CFD ENGINEER, MARUTEE DESIGN AND ENGINEERING')
-        st.write('SEPT 22, 2021 – TO DATE')
-        st.write('• Performed flow and thermal simulations to optimize designs and enhance performance.')
-        st.write('• Proficient in Excel for efficient analysis and decision-making.')
+        with st.container():
+            st.markdown('<div style="text-align:right"><h2>Skills & Abilities</h2></div',unsafe_allow_html=True)
+            st.write("<div style='text-align: right'>Programming: Python</div>", unsafe_allow_html=True)
+            st.write("<div style = 'text-align:right'>Libraries: Pandas, NumPy, Matplotlib, Scikit-learn</div>",unsafe_allow_html=True)
+            st.write("<div style = 'text-align:right'>Databases: MySQL, MS SQL</div>",unsafe_allow_html=True)
+            st.write("<div style = 'text-align:right'>Visualization: Excel, PowerPoint, Power BI</div>",unsafe_allow_html=True)
+            #st.write('Databases: MySQL, MS SQL')
+            
+            #st.write('Visualization: Excel, PowerPoint, Power BI')
+            st.write("<div style = 'text-align:right'>Operating Systems: Windows</div>",unsafe_allow_html=True)
+            #st.write('Operating Systems: Windows')
+        
+    with right:
+  
+        with st.container():
+            st.markdown('## Projects')
+            st.write("<div style='text-align: left'>Car price prediction using Linear regression.</div>", unsafe_allow_html=True)
+            st.write("<div style='text-align: left'>Heat attack prediction using logistic regression.</div>", unsafe_allow_html=True)
+            st.write("<div style='text-align: left'>Exploratory data analysis on Games sales with charts using POWERBI.</div>", unsafe_allow_html=True)
+            st.write("<div style='text-align: left'>Engineering Project - I designed a cassette ceiling air conditioning system that includes integrated lighting, speakers, and a projector, with voice-activated features.</div>", unsafe_allow_html=True)
 
-    with st.container():
-        st.markdown('## Courses')
-        st.write('### Google Data Analytics Professional Certificate, Coursera')
-        st.write('### Python 101 for Data Science, Cognitiveclass.ai, Powered by IBM')
-        st.write('### Vehicle Dynamics - ETG')
+
+    with left:
+        with st.container():
+            st.markdown("<div style ='text-align:right'><h2>Education</h2></div>",unsafe_allow_html=True)
+            st.write("<div style='text-align: right'>VEMANA INSTITUTE OF TECHNOLOGY</div>", unsafe_allow_html=True)
+            st.write("<div style='text-align: right'>BACHELOR’S IN MECHANICAL ENGINEERING</div>", unsafe_allow_html=True)
+            st.write("<div style='text-align: right'>Scored CGPA of 7.</div>", unsafe_allow_html=True)
+        st.write("")
+        st.write("")
+
+    with right:
+        with st.container():
+            st.markdown('## Experience')
+            st.write("<div style = 'text-align':left'>CFD ENGINEER, MARUTEE DESIGN AND ENGINEERING</div>",unsafe_allow_html=True)
+            st.write("<div style = 'text-align:left'>SEPT 22, 2021 – TO DATE</div>",unsafe_allow_html=True)
+            st.write("<div style ='text-align:left'> Performed flow and thermal simulations to optimize designs and enhance performance</div>",unsafe_allow_html=True)
+            st.write("<div style = 'text-align:left'>Proficient in Excel for efficient analysis and decision-making</div>",unsafe_allow_html=True)
+    with left:
+        with st.container():
+            st.markdown("<div style = 'text-align:right'><h2>Courses</h2></div>",unsafe_allow_html=True)
+            st.write("<div style = 'text-align:right'>Google Data Analytics Professional Certificate, Coursera</div>",unsafe_allow_html=True)
+            st.write("<div style = 'text-align:right'>Python 101 for Data Science, Cognitiveclass.ai, Powered by IBM</div>",unsafe_allow_html=True)
+            st.write("<div style = 'text-align:right'>Vehicle Dynamics - ETG</div>",unsafe_allow_html=True)
 
 
 
